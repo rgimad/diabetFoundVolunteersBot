731379f70ab94e30b9b50e07e357cca73fbee4cc
rgimad <rustem.gimadutdinov@gmail.com>
2023-09-02 16:40:38 +0300

    added fio

--- bot_main.py
+++ bot_main.py
@@ -1,6 +1,8 @@
 import telebot, sqlite3, hashlib
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
+fio_dict = dict()
+
 with open('token.txt', 'r') as file:
     token = file.read().replace('\n', '')
 bot = telebot.TeleBot(token)
@@ -17,9 +19,28 @@ def start_message(message):
 def help_message(message):
     bot.send_message(message.chat.id, '')
 
-# def message_reply(message):
-#     if message.text=="Начать анкетирование!":
-#         bot.send_message(message.chat.id, "")
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text == "Начать анкетирование!":
+        bot.send_message(message.from_user.id, "Введите фамилию: ")
+        bot.register_next_step_handler(message, get_surname)
+
+def get_surname(message):
+    global fio_dict
+    fio_dict[message.from_user.id] = message.text
+    bot.send_message(message.from_user.id, 'Введите имя:')
+    bot.register_next_step_handler(message, get_name)
+
+def get_name(message):
+    global fio_dict
+    fio_dict[message.from_user.id] += str(" " + message.text)
+    bot.send_message(message.from_user.id, 'Введите отчество:')
+    bot.register_next_step_handler(message, get_patronymic)
+
+def get_patronymic(message):
+    global fio_dict
+    fio_dict[message.from_user.id] += str(" " + message.text)
+    bot.send_message(message.from_user.id, fio_dict.get(message.from_user.id))
 
 
 bot.polling()
