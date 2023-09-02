6653c985174842edd4f44c8f4ab408365b9c3df4
rgimad <rustem.gimadutdinov@gmail.com>
2023-09-02 14:54:57 +0300

    fix start button size

--- bot_main.py
+++ bot_main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    keyboard = ReplyKeyboardMarkup()
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     button = KeyboardButton(text="Начать анкетирование!")
     keyboard.add(button)
     # Send the keyboard as a reply to the /start command
@@ -16,6 +16,10 @@ def start_message(message):
 @bot.message_handler(commands=['info'])
 def help_message(message):
     bot.send_message(message.chat.id, '')
+
+# def message_reply(message):
+#     if message.text=="Начать анкетирование!":
+#         bot.send_message(message.chat.id, "")
 
 
 bot.polling()
